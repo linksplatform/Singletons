f4e775a46df870edfa21cfcbd7fb8f4469828392
Konstantin Dyachenko <drakonard@gmail.com>
2019-09-12 21:28:32 +0300

    Ignore MonoDevelop false alarmed warnings.

--- Singleton[T].cs
+++ Singleton[T].cs
@@ -5,6 +5,7 @@ using Platform.Collections.Lists;
 using Platform.Reflection;
 
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning disable RECS0108 // Warns about static fields in generic types
 
 namespace Platform.Singletons
 {
