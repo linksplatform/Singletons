8a8c8be9b89429881dbb14bb3582a539103b8f6b
Konstantin Diachenko <drakonard@gmail.com>
2020-02-23 18:05:13 +0300

    Added test for Singleton struct.

---
+++ csharp/Platform.Singletons.Tests/SingletonTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace Platform.Singletons.Tests
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void TwoValuesAreTheSameTest()
+        {
+            var value1 = Singleton.Get(() => 1);
+            var value2 = Singleton.Get(() => 1);
+            Assert.Equal(value1, value2);
+        }
+
+        // Looks like ILBytes do not help here
+        //[Fact]
+        //public void TwoFunctionsAreTheSameTest()
+        //{
+        //    //Func<Func<int>> factory = () => () => 1;
+        //    var func1 = Singleton.Get<Func<int>>(() => () => 1);
+        //    var func2 = Singleton.Get<Func<int>>(() => () => 1);
+        //    Assert.Equal(func1, func2);
+        //}
+    }
+}
