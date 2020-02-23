4ba10c72e90868272caf236cbdab723524d25fba
Konstantin Diachenko <drakonard@gmail.com>
2020-02-23 18:24:56 +0300

    Added tests for Default class.

---
+++ csharp/Platform.Singletons.Tests/DefaultTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+
+namespace Platform.Singletons.Tests
+{
+    public class DefaultTests
+    {
+        [Fact]
+        public void StructInstanceTest()
+        {
+            Assert.Equal(0, Default<int>.Instance);
+        }
+
+        [Fact]
+        public void ClassInstanceTest()
+        {
+            Assert.NotNull(Default<object>.Instance);
+        }
+
+        [Fact]
+        public void StructThreadInstanceTest()
+        {
+            Assert.Equal(0, Default<int>.ThreadInstance);
+        }
+
+        [Fact]
+        public void ClassThreadInstanceTest()
+        {
+            Assert.NotNull(Default<object>.ThreadInstance);
+        }
+    }
+}
