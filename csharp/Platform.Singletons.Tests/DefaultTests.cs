212c176f8e5e5c7dcc29a0aaeee159a797b663fd
FreePhoenix <freephoenix888@gmail.com>
2021-11-19 16:08:41 +0600

    Remove XML Documentation for tests and benchmarks

--- csharp/Platform.Singletons.Tests/DefaultTests.cs
+++ csharp/Platform.Singletons.Tests/DefaultTests.cs
@@ -2,56 +2,26 @@ using Xunit;
 
 namespace Platform.Singletons.Tests
 {
-    /// <summary>
-    /// <para>
-    /// Represents the default tests.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     public class DefaultTests
     {
-        /// <summary>
-        /// <para>
-        /// Tests that struct instance test.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [Fact]
         public void StructInstanceTest()
         {
             Assert.Equal(0, Default<int>.Instance);
         }
 
-        /// <summary>
-        /// <para>
-        /// Tests that class instance test.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [Fact]
         public void ClassInstanceTest()
         {
             Assert.NotNull(Default<object>.Instance);
         }
 
-        /// <summary>
-        /// <para>
-        /// Tests that struct thread instance test.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [Fact]
         public void StructThreadInstanceTest()
         {
             Assert.Equal(0, Default<int>.ThreadInstance);
         }
 
-        /// <summary>
-        /// <para>
-        /// Tests that class thread instance test.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [Fact]
         public void ClassThreadInstanceTest()
         {
--- csharp/Platform.Singletons.Tests/GlobalTests.cs
+++ csharp/Platform.Singletons.Tests/GlobalTests.cs
@@ -2,20 +2,8 @@ using Xunit;
 
 namespace Platform.Singletons.Tests
 {
-    /// <summary>
-    /// <para>
-    /// Represents the global tests.
-    /// </para>
-    /// <para></para>
-    /// </summary>
     public class GlobalTests
     {
-        /// <summary>
-        /// <para>
-        /// Tests that trash is null test.
-        /// </para>
-        /// <para></para>
-        /// </summary>
         [Fact]
         public void TrashIsNullTest()
         {
